870af2e9ca472e5cd85ad95416ab3d4816ea3184
vlad1slavG <han3and0rs0n@gmail.com>
2024-06-16 15:34:36 +0300

    added footer and nav-bar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, FooterComponent, NavBarComponent],
 })
-export class AppComponent {
-  title = 'onboarding-form';
-}
+export class AppComponent {}
